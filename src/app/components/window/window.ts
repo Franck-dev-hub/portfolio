f0b18da3620a42e0935b0d4ca1c293861a5a23a2
Franck-dev-hub <franck.spadotto@yahoo.com>
2026-04-06 01:35:01 +0200

    feat(window): init window project display idea

---
+++ src/app/components/window/window.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-window",
+  imports: [],
+  templateUrl: "./window.html",
+  styleUrl: "./window.css",
+})
+export class Window {}
