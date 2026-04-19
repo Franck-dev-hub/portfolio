5f5f1359f51a13a40f20c8200800c5546fa831fa
Franck-dev-hub <franck.spadotto@yahoo.com>
2026-04-19 18:06:41 +0200

    update: finish to export components

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,14 @@ import { Header } from "./components/header/header";
 import { Intro } from "./components/windows/intro/intro";
 import { Projects } from "./components/windows/projects/projects";
 import { Stacks } from "./components/windows/stacks/stacks";
+import { IntroList } from "./components/list/intro-list/intro-list";
+import { StackList } from "./components/list/stack-list/stack-list";
+import { ProjectsList } from "./components/list/projects-list/projects-list";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: "app-root",
-  imports: [Header, Intro, Projects, Stacks],
+  imports: [Header, Intro, Projects, Stacks, IntroList, StackList, ProjectsList, Footer],
   templateUrl: "./app.html",
   styleUrl: "./app.css",
 })
